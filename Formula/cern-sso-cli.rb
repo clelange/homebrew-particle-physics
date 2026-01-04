a0253daa53bf12d59ab6fbb9e2bc10545e20715c
Clemens Lange <clemens.lange@cern.ch>
2026-01-04 21:12:45 +0100

    Use pre-built binaries from releases

--- Formula/cern-sso-cli.rb
+++ Formula/cern-sso-cli.rb
@@ -1,15 +1,24 @@
 class CernSsoCli < Formula
   desc "Command-line interface to obtain CERN SSO cookies and tokens"
   homepage "https://github.com/clelange/cern-sso-cli"
-  url "https://github.com/clelange/cern-sso-cli/archive/refs/tags/v0.20.0.tar.gz"
-  sha256 "de72dfa638451fcf16581ea42d61106372788263b2e2e326e1588e8ca6a3f8b4"
+  version "0.20.0"
   license "GPL-3.0-only"
 
-  depends_on "go" => :build
+  on_macos do
+    if Hardware::CPU.intel?
+      url "https://github.com/clelange/cern-sso-cli/releases/download/v0.20.0/cern-sso-cli-darwin-amd64"
+      sha256 "d64786c97113555b135a18fdaaa89605859b4b1286eaf281b24c85780680e656"
+    elsif Hardware::CPU.arm?
+      url "https://github.com/clelange/cern-sso-cli/releases/download/v0.20.0/cern-sso-cli-darwin-arm64"
+      sha256 "3d995ecc753e43436159bc9f920d7b72d112dfc789fb1c5f1c328b2740e1ebf8"
+    end
+  end
+
   depends_on "libfido2" => :optional
 
   def install
-    system "go", "build", *std_go_args(ldflags: "-s -w")
+    bin.install "cern-sso-cli-darwin-amd64" => "cern-sso-cli" if Hardware::CPU.intel?
+    bin.install "cern-sso-cli-darwin-arm64" => "cern-sso-cli" if Hardware::CPU.arm?
 
     generate_completions_from_executable(bin/"cern-sso-cli", "completion")
   end
