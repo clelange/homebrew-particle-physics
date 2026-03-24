0e5a96adf39bbcad4e6bc34139ebca33c021e707
Clemens Lange <clemens.lange@cern.ch>
2026-03-24 11:15:58 +0100

    Account for migration of cernopendata-client-go from clelange to cernopendata

--- Formula/cernopendata-client.rb
+++ Formula/cernopendata-client.rb
@@ -1,15 +1,15 @@
 class CernopendataClient < Formula
   desc "CERN Open Data Client"
-  homepage "https://github.com/clelange/cernopendata-client-go"
+  homepage "https://github.com/cernopendata/cernopendata-client-go"
   version "0.9.0"
   license "GPL-3.0-only"
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/clelange/cernopendata-client-go/releases/download/v0.9.0/cernopendata-client-darwin-amd64"
+      url "https://github.com/cernopendata/cernopendata-client-go/releases/download/v0.9.0/cernopendata-client-darwin-amd64"
       sha256 "4bd1e5ed5d83c06dedbae6f4333da25825e79957a624ee2a6e3c152fa4096174"
     elsif Hardware::CPU.arm?
-      url "https://github.com/clelange/cernopendata-client-go/releases/download/v0.9.0/cernopendata-client-darwin-arm64"
+      url "https://github.com/cernopendata/cernopendata-client-go/releases/download/v0.9.0/cernopendata-client-darwin-arm64"
       sha256 "8679e95295153dd184aed2d650fd22970afb624f493b04b4aade997e4a1af4f3"
     end
   end
